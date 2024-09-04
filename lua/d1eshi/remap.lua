6bcd08aa2e70217563dc65b7314c1268d5353d7b
d1eshi <silva.diegof03@gmail.com>
2024-09-04 13:11:10 -0300

    remaps from nvim base fro nvim-tree

--- lua/d1eshi/remap.lua
+++ lua/d1eshi/remap.lua
@@ -1,6 +1,24 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
+local function map(mode, lhs, rhs, opts)
+	local options = { noremap = true, silent = true }
+	if opts then
+		options = vim.tbl_extend("force", options, opts)
+	end
+	vim.keymap.set(mode, lhs, rhs, options)
+end
+
+-- Toggle NvimTree
+map("n", "<C-n>", ":NvimTreeToggle<CR>")
+-- -- Focus NvimTree
+map("n", "<leader>e", ":NvimTreeFocus<CR>")
+
+vim.keymap.set("x", "<leader>p", [["_dP]])
+
+
+-- Opcional: Mapeo para alternar el wrap de texto
+-- vim.api.nvim_set_keymap('n', '<leader>w', ':set wrap!<CR>', { noremap = true, silent = true })
 
 --local function map(mode, lhs, rhs, opts)
 --  local options = { noremap = true, silent = true }
