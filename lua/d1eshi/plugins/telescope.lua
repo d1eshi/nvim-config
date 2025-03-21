5c6ddeaedf6e507bbd54c139a343e12fa966bad8
Diego Silva <d1eshi@Diegos-MacBook-Air.local>
2025-03-20 22:03:28 -0300

    fix typescript integrations and [add] linting for servers

--- lua/d1eshi/plugins/telescope.lua
+++ lua/d1eshi/plugins/telescope.lua
@@ -1,52 +1,50 @@
 return {
 
-   "nvim-telescope/telescope.nvim",
+	"nvim-telescope/telescope.nvim",
 
-    tag = "0.1.x",
+	tag = "0.1.x",
 
-    dependencies = {
-     "nvim-lua/plenary.nvim",
-     { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
-     "nvim-tree/nvim-web-devicons",
-    },
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+		"nvim-tree/nvim-web-devicons",
+	},
 
-    config = function()
-       local telescope = require('telescope') 
-       local actions = require('telescope.actions') 
-      
-       telescope.setup({
-         defaults = {
-           path_display = { "smart" },
-           mappings = {
-            i = {
-              ["<C-k>"] = actions.move_selection_previous, 
-              ["<C-j>"] = actions.move_selection_next, 
-              ["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist, 
-            },
-           },
-         }
-       })
+	config = function()
+		local telescope = require("telescope")
+		local actions = require("telescope.actions")
 
-       telescope.load_extension("fzf")
+		telescope.setup({
+			defaults = {
+				path_display = { "smart" },
+				mappings = {
+					i = {
+						["<C-k>"] = actions.move_selection_previous,
+						["<C-j>"] = actions.move_selection_next,
+						["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
+					},
+				},
+			},
+		})
 
-       local builtin = require('telescope.builtin')
+		telescope.load_extension("fzf")
 
-       vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-       vim.keymap.set('n', '<C-p>', builtin.git_files, {})
-       vim.keymap.set('n', '<leader>fws', function()
-           local word = vim.fn.expand("<cword>")
-           builtin.grep_string({ search = word })
-       end)
-       vim.keymap.set('n', '<leader>fWs', function()
-           local word = vim.fn.expand("<cWORD>")
-           builtin.grep_string({ search = word })
-       end)
-       vim.keymap.set('n', '<leader>fs', function()
-           builtin.grep_string({ search = vim.fn.input("Grep > ") })
-       end)
-       vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
-       vim.keymap.set('n', '<leader>fc', "<cmd>Telescope grep_string<cr>", { desc = "Find string under cursor cwd"})
+		local builtin = require("telescope.builtin")
 
-
-    end
+		vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
+		vim.keymap.set("n", "<C-p>", builtin.git_files, {})
+		-- vim.keymap.set("n", "<leader>fws", function()
+		-- 	local word = vim.fn.expand("<cword>")
+		-- 	builtin.grep_string({ search = word })
+		-- end)
+		vim.keymap.set("n", "<leader>fWs", function()
+			local word = vim.fn.expand("<cWORD>")
+			builtin.grep_string({ search = word })
+		end)
+		vim.keymap.set("n", "<leader>fs", function()
+			builtin.grep_string({ search = vim.fn.input("Grep > ") })
+		end)
+		vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
+		vim.keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>", { desc = "Find string under cursor cwd" })
+	end,
 }
